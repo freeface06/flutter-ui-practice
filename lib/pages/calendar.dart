7e47143ce6e302e43fb4582744606bc0d9865265
Hojung Lee <freeface06@naver.com>
2023-04-20 19:59:40 +0900

    day2

--- lib/pages/calendar.dart
+++ lib/pages/calendar.dart
@@ -1,18 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-void main() => runApp(const CalendarPage());
+void main() {
+  runApp(const CalendarPage());
+}
 
-class CalendarPage extends StatelessWidget {
+class CalendarPage extends StatefulWidget {
   const CalendarPage({Key? key}) : super(key: key);
 
   @override
+  State<StatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<CalendarPage> {
+  final CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+
+  void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
+    setState(() {
+      _selectedDay = selectedDay;
+      _focusedDay = focusedDay;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('CalendarPage'),
-        ),
-      ),
+    return TableCalendar(
+      calendarFormat: _calendarFormat,
+      focusedDay: _focusedDay,
+      firstDay: DateTime.utc(2021, 1, 1),
+      lastDay: DateTime.utc(2030, 12, 31),
+      selectedDayPredicate: (day) {
+        return isSameDay(_selectedDay, day);
+      },
+      onDaySelected: _onDaySelected,
     );
   }
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,46 +1,90 @@
-import 'package:flutter/material.dart';
 import 'dart:convert';
+
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-void main() => runApp(const HomePage());
+void main() {
+  runApp(const HomePage());
+}
 
-class HomePage extends StatelessWidget {
+class Post {
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+
+  Post(
+      {required this.userId,
+      required this.id,
+      required this.title,
+      required this.body});
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      userId: json['userId'] as int,
+      id: json['id'] as int,
+      title: json['title'] as String,
+      body: json['body'] as String,
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
-  Future<List<String>> fetchData() async {
+  @override
+  State<HomePage> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<HomePage> {
+  late Future<List<Post>> _futurePosts;
+
+  @override
+  void initState() {
+    super.initState();
+    _futurePosts = _fetchPosts();
+  }
+
+  Future<List<Post>> _fetchPosts() async {
     final response =
         await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
     if (response.statusCode == 200) {
-      final data = jsonDecode(response.body) as List<dynamic>;
-      return data.map((item) => item['title'] as String).toList();
+      final List<dynamic> rawPosts = jsonDecode(response.body);
+      return rawPosts.map((json) => Post.fromJson(json)).toList();
     } else {
-      throw Exception('Failed to fetch data');
+      throw Exception('Failed to load posts');
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Flutter Demo',
       home: Scaffold(
-        body: FutureBuilder<List<String>>(
-          future: fetchData(),
+        appBar: null,
+        body: FutureBuilder<List<Post>>(
+          future: _futurePosts,
           builder: (context, snapshot) {
             if (snapshot.hasData) {
-              return ListView.builder(
-                  itemCount: snapshot.data!.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      title: Text(snapshot.data![index]),
-                    );
-                  });
+              final List<Post> posts = snapshot.data!;
+              return ListView.separated(
+                itemCount: posts.length,
+                itemBuilder: (context, index) {
+                  final Post post = posts[index];
+                  return ListTile(
+                    leading: const Icon(Icons.local_post_office),
+                    title: Text(post.title),
+                    subtitle: Text(post.body),
+                    trailing: const Icon(Icons.arrow_right),
+                  );
+                },
+                separatorBuilder: (BuildContext context, int index) =>
+                    const Divider(),
+              );
             } else if (snapshot.hasError) {
-              return Center(
-                child: Text('${snapshot.error}'),
-              );
+              return Text('${snapshot.error}');
             } else {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
+              return const Center(child: CircularProgressIndicator());
             }
           },
         ),
